6ab7598ddda397603d3eaa4aa09cb883d6215573
blueebeettle <dalwadipriyesh@gmail.com>
2024-06-08 17:07:09 +0530

    more styling

--- script.js
+++ script.js
@@ -16,16 +16,18 @@ const checkWin = () => {
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
+        [0, 3, 6,],
+        [1, 4, 7,],
+        [2, 5, 8,],
+        [0, 4, 8,],
+        [2, 4, 6,],
     ]
     wins.forEach(e => {
         if ((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[2]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[0]].innerText !== "")) {
             document.querySelector(".info").innerText = boxtexts[e[0]].innerText + " Won"
             gameOver = true
+            document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width = "200px"
+            gameOverBgm.play()
         }
     })
 
@@ -47,4 +49,16 @@ Array.from(boxes).forEach(element => {
             }
         }
     })
+})
+
+// add onClick listener to reset button
+reset.addEventListener("click", () => {
+    let boxtext = document.querySelectorAll(".boxtext");
+    Array.from(boxtext).forEach(element => {
+        element.innerText = ""
+    });
+    turn = "X";
+    gameOver = false
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width = "0px"
 })
